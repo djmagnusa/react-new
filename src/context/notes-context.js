7dd12284e255f320bf7c5179e50365f6f1534221
Pratush <pratush.bh@gmail.com>
2021-08-29 18:17:15 +0530

    Add react context

---
+++ src/context/notes-context.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotesContext = React.createContext() //we can provide on argument which will be the default value for the argument
+
+export { NotesContext as default }
